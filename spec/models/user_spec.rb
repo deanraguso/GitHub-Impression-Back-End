e38c183238af3249273babb919b4cb368b5ed003
dean <deanraguso@gmail.com>
2021-05-09 20:33:01 +1000

    Created: Basic user create method works under good inputs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe '' do
+  describe "can be created" do
+    # Use let! to avoid lazy loading
+    let!(:user) { User.create( 
+                                username: "user1", 
+                                email: "user1@sample.com",
+                                password: "password1"
+                                ) }
     
+    it "creates a user with a default favourites list of 0 length" do
+      expect(User.count).to eq(1)
+    end
   end
 end
