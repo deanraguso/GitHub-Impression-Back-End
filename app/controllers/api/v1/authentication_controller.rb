ff01951e0962f73255ae3793183c34b7836c9625
dean <deanraguso@gmail.com>
2021-05-10 10:43:04 +1000

    Altered: Rails routes to exclude native devise session controller

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,3 +1,13 @@
 class Api::V1::AuthenticationController < ApplicationController
-    
+    def create
+        token = AuthenticationTokenService.call(get_user.id)
+
+        params.require(:password)
+        render json: {token: token}, status: :created
+    end
+
+    private
+    def get_user
+        @user ||= User.find_by(username: params.require(:username))
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'api/v1'
+  devise_for :users, path: 'api/v1', only: [:passwords, :registration]
 end
