fe483a37dd0d9d4d9ea85b9d0180de4e83a2e07e
dean <deanraguso@gmail.com>
2021-05-10 18:08:27 +1000

    Created: Update path and action

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,11 +2,15 @@ class Api::V1::FavoritesController < ApplicationController
   before_action :user
   
   def create
+    # appends to existing array with the one passed.
     user.update(favorites: user.favorites + params[:favorites])
     render json: user.favorites, status: :created
   end
 
   def update
+    # replaces the existing array with the one passed.
+    user.update(favorites: params[:favorites])
+    render json: user.favorites, status: :created
   end
 
   def destroy
